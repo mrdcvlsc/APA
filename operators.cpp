a201f4d5cd1f997de718305f00a3ee6d158c8a01
mrdcvlsc <59404502+mrdcvlsc@users.noreply.github.com>
2020-10-30 14:26:09 +0800

    new: comparison operators, initialization, (++,--)
    The following comparison operators are added:
        ==, !=, >, <=, >=
    The declaration of bigint variables are improved:
        BIGINT yourVar = "823782323"; // is no allowed
    
    The old declaration is still allowed :    
        BIGINT yourVar("823782323");   //still allowed 
    
    Increment and Decrement operators is also added
    	supports for postfix and prefix

--- operators.cpp
+++ operators.cpp
@@ -12,23 +12,6 @@ istream& operator>>(istream &in, BIGINT &value) {
 }
 // -------------------------------------------------------
 
-
-//  ---------- Assignment Operator ---------- 
-BIGINT& BIGINT::operator=(string value){
-	check(value);
-	data=value;
-	return *this;
-}
-/*
-BIGINT BIGINT::operator=(const char* value){
-	BIGINT result;
-	result.data=value;
-	check(result.data);
-	return result;
-}*/
-// ------------------------------------------
-
-
 // -------------------- Arithmetic Operators --------------------
 BIGINT BIGINT::operator+(const BIGINT& bigintNumber){
 	BIGINT result;
@@ -163,61 +146,109 @@ BIGINT BIGINT::operator*(const BIGINT& bigintNumber){
 
 // -------------------- Comparison Operators --------------------
 bool operator<(const BIGINT& bintleft,const BIGINT& bintright){
-		BIGINT left  = bintleft,
-		 	   right = bintright;
+	BIGINT left  = bintleft,
+	 	   right = bintright;
 
-		bool leftIsPositive = left.data[0] != '-' ? true : false;
-		bool rightIsPositive = right.data[0] != '-' ? true : false;
+	bool leftIsPositive = left.data[0] != '-' ? true : false;
+	bool rightIsPositive = right.data[0] != '-' ? true : false;
 
-		int leftLength  =  left.data.size();	
-		int rightLength = right.data.size();
+	int leftLength  =  left.data.size();	
+	int rightLength = right.data.size();
 
-		if(!leftIsPositive){
-			left.data.erase(0,1);
-			leftLength--;
-		}
-
-		if(!rightIsPositive){
-			right.data.erase(0,1);
-			rightLength--;
-		}
-
-		if(!leftIsPositive and rightIsPositive) 
-			return true;
-		else if(leftIsPositive and !rightIsPositive){ 
-			cout<<"\njust chilin "<<endl;
-			return false;
-		}
-		else if(leftIsPositive and rightIsPositive){	
-			if(leftLength<rightLength)
-				return true;
-			else if(leftLength == rightLength){
-				for(int i=0;i<leftLength;++i){
-					if(left.data[i] == right.data[i])
-						continue;
-					else if(left.data[i] < right.data[i])
-						return true;
-					else return false;
-				}
-			}
-			else
-				return false;
-		}
-		else if(!leftIsPositive and !rightIsPositive){  
-			if(leftLength>rightLength)
-				return true;
-			else if(leftLength == rightLength){
-				for(int i=0;i<leftLength;++i){
-					if(left.data[i] == right.data[i])
-						continue;
-					else if(left.data[i] > right.data[i])
-						return true;
-					else return false;
-				}
-			}
-			else
-				return false;
-		}
-		return false;
+	if(!leftIsPositive){
+		left.data.erase(0,1);
+		leftLength--;
 	}
+
+	if(!rightIsPositive){
+		right.data.erase(0,1);
+		rightLength--;
+	}
+
+	if(!leftIsPositive and rightIsPositive) 
+		return true;
+	else if(leftIsPositive and !rightIsPositive)
+		return false;
+	else if(leftIsPositive and rightIsPositive){	
+		if(leftLength<rightLength)
+			return true;
+		else if(leftLength == rightLength){
+			for(int i=0;i<leftLength;++i){
+				if(left.data[i] == right.data[i])
+					continue;
+				else if(left.data[i] < right.data[i])
+					return true;
+				else return false;
+			}
+		}
+		else
+			return false;
+	}
+	else if(!leftIsPositive and !rightIsPositive){  
+		if(leftLength>rightLength)
+			return true;
+		else if(leftLength == rightLength){
+			for(int i=0;i<leftLength;++i){
+				if(left.data[i] == right.data[i])
+					continue;
+				else if(left.data[i] > right.data[i])
+					return true;
+				else return false;
+			}
+		}
+		else
+			return false;
+	}
+	return false;
+}
+bool operator>(const BIGINT& bintleft,const BIGINT& bintright){
+	if(bintleft.data==bintright.data)
+		return false;
+	if(bintleft<bintright)
+		return false;
+	return true;
+}
+bool operator<=(const BIGINT& bintleft,const BIGINT& bintright){
+	if(bintleft.data==bintright.data)
+		return true;
+	return bintleft<bintright;
+}
+bool operator>=(const BIGINT& bintleft,const BIGINT& bintright){
+	if(bintleft.data==bintright.data)
+		return true;
+	return bintleft>bintright;
+}
+bool operator==(const BIGINT& bintleft,const BIGINT& bintright){
+	if(bintleft.data==bintright.data)
+		return true;
+	return false;
+}
+bool operator!=(const BIGINT& bintleft,const BIGINT& bintright){
+	if(bintleft.data!=bintright.data)
+		return true;
+	return false;
+}
 // --------------------------------------------------------------
+
+// ----------------- INCREMENT/DECREMENT OPERATORS ---------------------------
+  //pre-fix
+	BIGINT BIGINT::operator++(){
+		this->data = (BIGINT(this->data)+BIGINT("1")).data;
+		return BIGINT(data);
+	}
+	BIGINT BIGINT::operator--(){
+		this->data = (BIGINT(this->data)-BIGINT("1")).data;
+		return BIGINT(data);
+	}
+
+  //post-fix
+	BIGINT BIGINT::operator++(int){
+        this->data = (BIGINT(this->data)+BIGINT("1")).data;
+        return BIGINT(this->data)-BIGINT("1"); 
+    }
+
+    BIGINT BIGINT::operator--(int){
+        this->data = (BIGINT(this->data)-BIGINT("1")).data;
+        return BIGINT(this->data)+BIGINT("1"); 
+    } 
+// --------------------------------------------------------------------------
