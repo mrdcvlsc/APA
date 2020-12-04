dc87fcfaa93c5201e2604aba836157b5f88bd9e4
mrdcvlsc <59404502+mrdcvlsc@users.noreply.github.com>
2020-12-04 08:48:49 +0800

    Update errorhandling.cpp

--- source/errorhandling.cpp
+++ source/errorhandling.cpp
@@ -42,33 +42,3 @@ void bignum::check(string &value){
 		}
 	}
 }
-
-// you see I'm not so confident with my implementation of division operation thats why this is here
-void bignum::internal_division_check(const bignum& dividen, const bignum& divisor) const{
-
-	if(divisor=="0"){
-		cout<<"\nERROR - bignum [internal division]: ("<<dividen<<'/'<<divisor<<") dividing by zero is not possible"<<endl;
-		exit(1);
-	}
-
-	if(divisor>dividen){
-		cout<<"\nERROR - bignum [internal division]:\n\n\t"<<endl;
-		cout<<"\t internal_division() do not support dividing 'dividen' to a greater 'divisor'"<<endl;
-		cout<<"\t your divisor ("<<divisor<<") is greater than your dividen ("<<dividen<<"."<<endl;
-		exit(1);
-	}
-	
-	if(divisor<"0"){	
-		cout<<"\nERROR - bignum [internal division]:"; 
-		cout<<"\n\t internal_division() do not support division to a negative value"<<endl;
-		cout<<"\t the 'DIVISOR' is ("<<divisor<<") lessthan zero(0). "<<endl;
-		exit(1);
-	}
-
-	if(dividen<"0"){
-		cout<<"\nERROR - bignum [internal division]:"<<endl; 
-	    cout<<"\t internal_division() do not support division to a negative value"<<endl;
-		cout<<"\t the 'DIVIDEN' is ("<<dividen<<") lessthan zero(0). "<<endl;
-		exit(1);
-	}
-}
