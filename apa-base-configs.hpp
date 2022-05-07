e4ce9fadc044b5223d165fb7f71bb183afcb3435
mrdcvlsc <mrdcvlsc@gmail.com>
2022-05-07 12:45:01 +0800

    isolated macro configurations and fixed some print formats for ubint limbs

---
+++ apa-base-configs.hpp
@@ -0,0 +1,58 @@
+#ifndef APA_BASE_CONFIGS_HPP
+#define APA_BASE_CONFIGS_HPP
+
+// #define _BASE2_64
+
+#ifdef _BASE2_64
+    #if (__MINGW64__ || __MINGW64)
+        #define PRINT_LIMBHEX_NOPAD "%llx"
+        #define PRINT_LIMBHEX "%16llx"
+        #define PRINT_LIMBHEX_SPACED " %16llx"
+    #elif (__clang__ || __GNUC__ || __GNUG__)
+        #define PRINT_LIMBHEX_NOPAD "%lx"
+        #define PRINT_LIMBHEX "%016lx"
+        #define PRINT_LIMBHEX_SPACED " %016lx"
+    #else
+        #error not supported
+    #endif
+#elif defined(_BASE2_32)
+    #if (__MINGW64__ || __MINGW64)
+        #define PRINT_LIMBHEX_NOPAD "%lx"
+        #define PRINT_LIMBHEX "%08lx"
+        #define PRINT_LIMBHEX_SPACED " %08lx"
+    #elif (__clang__ || __GNUC__ || __GNUG__)
+        #define PRINT_LIMBHEX_NOPAD "%x"
+        #define PRINT_LIMBHEX "%08x"
+        #define PRINT_LIMBHEX_SPACED " %08x"
+    #else
+        #error not supported
+    #endif
+#elif defined(_BASE2_16)
+    #if (__MINGW64__ || __MINGW64)
+        #define PRINT_LIMBHEX_NOPAD "%x"
+        #define PRINT_LIMBHEX "%04x"
+        #define PRINT_LIMBHEX_SPACED " %04x"
+    #elif (__clang__ || __GNUC__ || __GNUG__)
+        #define PRINT_LIMBHEX_NOPAD "%x"
+        #define PRINT_LIMBHEX "%04x"
+        #define PRINT_LIMBHEX_SPACED " %04x"
+    #else
+        #error not supported
+    #endif
+#endif
+
+namespace apa {
+    // 'limb_t' should always be double the size of 'base_t', this is to avoid overflows.
+    #ifdef _BASE2_64
+    typedef uint64_t base_t;
+    typedef __uint128_t limb_t;
+    #elif defined(_BASE2_32)
+    typedef uint32_t base_t;
+    typedef uint64_t limb_t;
+    #elif defined(_BASE2_16)
+    typedef uint16_t base_t;
+    typedef uint32_t limb_t;
+    #endif
+}
+
+#endif
