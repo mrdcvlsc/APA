8fc015493651e0053e918419c3111f14fa7cc6f1
mrdcvlsc <mrdcvlsc@gmail.com>
2022-08-01 01:07:48 +0800

    build fix 1

--- math.hpp
+++ math.hpp
@@ -16,20 +16,9 @@ namespace apa {
         }
         return base0 + base1;
     }
-
-    integer fibonacci_integer(size_t nth, integer b0 = 0, integer b1 = 1) {
-        if(nth < 2) {
-            return __INTEGER_ONE;
-        }
-        return fibonacci_template<integer>(nth, b0, b1);
-    }
-
-    bint fibonacci(size_t nth, bint b0 = 0, bint b1 = 1) {
-        if(nth < 2) {
-            return __BINT_ONE;
-        }
-        return fibonacci_template<bint>(nth, b0, b1);
-    }
+    
+    integer fibonacci_integer(size_t nth, integer b0 = 0, integer b1 = 1);
+    bint fibonacci(size_t nth, bint b0 = 0, bint b1 = 1);
 
     template<class T>
     T factorial_template(size_t n) {
@@ -44,13 +33,8 @@ namespace apa {
         return factorial;
     }
 
-    integer factorial_integer(size_t n) {
-        return factorial_template<integer>(n);
-    }
-
-    bint factorial(size_t n) {
-        return factorial_template<bint>(n);
-    }
+    integer factorial_integer(size_t n);
+    bint factorial(size_t n);
 }
 
 #endif
